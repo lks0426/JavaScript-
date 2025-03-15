6a375171211c51d55b2d41ef2b4ea86a92a13e63
qqw744 <sxz947@gmail.com>
2025-03-15 22:55:04 +0900

    0313作业

---
+++ 20250313/0313.js
@@ -0,0 +1,30 @@
+let character="Hello";
+console.log(character);
+character="World";
+console.log(character);
+
+let count=8;
+console.log(count+1);
+
+let rows=["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows[0]);
+rows[rows.length-1];
+console.log(rows[rows.length-1]);
+let pushed=rows.push("freeCodeCamp");
+console.log(pushed);
+let popped=rows.pop();
+console.log(popped);
+
+let cities=[ "London", "New York", "Mumbai" ];
+console.log(cities);
+cities[2]="Mexico City";
+console.log(cities);
+
+const character="#";
+const count=8;
+const rows=[];
+
+for (iterator; condition; iteration){
+    
+}
