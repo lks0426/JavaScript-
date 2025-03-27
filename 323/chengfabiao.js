c7ad5dc2f28a7a0e2dd0b37a969a0bdf635f10eb
Fitz_z <yiran.zhang.me@gmail.com>
2025-03-27 16:25:16 +0900

    冒泡排序

--- 323/chengfabiao.js
+++ 323/chengfabiao.js
@@ -1,7 +1,8 @@
 for (let x = 1; x <= 9; x++) {
     let hang = "";
     for (let y = 1; y <= x; y++) {
-      hang += `${x}×${y}=${x * y}\t`;
+    
+    hang += `${y} * ${x} = ${x * y}\t`;
     }
     console.log(hang);
   }
